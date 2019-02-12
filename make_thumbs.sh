805b3ca2eee444daa18831032b6e5ae9eb49fb25
Kyle Pollock <kyle.pollock7@gmail.com>
2019-02-11 21:52:06 -0600

    Slot in new images

--- make_thumbs.sh
+++ make_thumbs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ~/repos/oskar/static/img/women/
 for i in *.jpg; do convert $i -thumbnail 365x300^ -gravity center -extent 365x300 thumbs/$i; done
-for i in *.png; do convert $i -thumbnail 365x300^ -gravity center -extent 365x300 thumbs/$i; done
 cd ~/repos/oskar/static/img/men/
 for i in *.jpg; do convert $i -thumbnail 365x300^ -gravity center -extent 365x300 thumbs/$i; done
 cd ~/repos/oskar/static/img/children/
